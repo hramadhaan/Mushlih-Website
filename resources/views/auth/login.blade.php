2f79a459a5a394891d1558a1b0997d61e44b35d1
hramadhaan <hramadhaan20@gmail.com>
2019-11-22 10:23:53 +0700

    Update login.blade.php
    Memperbaiki Tampilan Login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,34 +1,31 @@
 @extends('layouts.app') @section('content')
 
-<div class="container">
-    <div class="columns">
-        <div class="column is-half">
-            <form action="{{ route('login') }}" method="POST">
-                @csrf
-                <div class="field">
-                    <label class="label">{{ __('E-Mail Address') }}</label>
-                    <div class="control">
-                        <input id="email" class="input @error('email') is-danger @enderror" type="email" placeholder="E-Mail" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-                    </div>
+<div class="column is-4 is-offset-4">
+    <div class="box">
+        <form action="{{ route('login') }}" method="POST">
+            @csrf
+            <div class="field">
+                <label class="label">Email</label>
+                <div class="control">
+                    <input type="email" name="email" id="email" class="input @error('email') si-danger @enderror" value="{{ old('email') }}" required="required" autocomplete="email" autofocus>
                 </div>
+            </div>
 
-                <div class="field">
-                    <label class="label">{{ __('Password') }}</label>
-                    <div class="control">
-                        <input id="password" class="input @error('password') is-danger @enderror" type="password" name="password" required autocomplete="current-password"> @error('password')
-                        <span class="icon is-small is-right">
-                            <i class="fas fa-exclamation-triangle"></i>
-                        </span> @enderror
-                    </div>
+            <div class="field">
+                <label class="label">Password</label>
+                <div class="control">
+                    <input type="password" name="password" id="password" class="input @error('password') si-danger @enderror" value="{{ old('password') }}" required="required" autocomplete="email">
                 </div>
+            </div>
 
-                <div class="field is-grouped">
-                    <div class="control">
-                        <button class="button is-link" type="submit">{{ __('Login')}}</button>
-                    </div>
+            <div class="field is-grouped">
+                <div class="control">
+                    <button class="button is-link" type="submit">{{ __('Login') }}</button>
                 </div>
-            </form>
-        </div>
+            </div>
+
+
+        </form>
     </div>
 </div>
 
